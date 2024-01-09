f763d2dbae62fc5eb8a7a7ac3031c950aee6b355
zouheirrhoumri <zouheir.rhoumri@gmail.com>
2024-01-09 14:12:23 +0100

    setting up the enviroment

---
+++ app/config/config.php
@@ -0,0 +1,15 @@
+<?php
+  // DB Params
+  define('DB_HOST', 'localhost');
+  define('DB_USER', 'root');
+  define('DB_PASS', '');
+  define('DB_NAME', 'wikimanager');
+
+  // App Root
+  define('APPROOT', dirname(dirname(__FILE__)));
+  // URL Root
+  define('URLROOT', 'http://localhost/WikiArcticle');
+  // Site Name
+  define('SITENAME', 'WikiArcticle');
+  // App Version
+  define('APPVERSION', '1.0.0');
